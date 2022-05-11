3c8e191fbf7444a8a9ee2d5d69354e33014ba91e
internacc <102132891+internacc@users.noreply.github.com>
2022-05-12 01:38:21 +0530

    commented Enter and Follow for easy deploying

--- src/Components/footer.js
+++ src/Components/footer.js
@@ -51,45 +51,45 @@ const Item = styled(Paper)(({ theme }) => ({
     textAlign: 'center',
     color: theme.palette.text.secondary,
 }));
-const Enter = () => {
-    const classes1 = useStyles();
-    return (<div>
-        <Box sx={{flexGrow: 1}}>
-            <div className={classes1.comp_disp}>
-                <div className='part1'>
-            <Typography variant="h3" component="div" gutterBottom sx={{ color: '#FFA900' }}>
-                <strong>DARE TO ENTER</strong>
-            </Typography>
-            <Button className={`'btn_prop' ${classes1.btn_prop}`}>
-                <Typography variant="h5" component="div">
-                JOIN OUR DISCORD
-                </Typography>
-            </Button></div>
+// const Enter = () => {
+//     const classes1 = useStyles();
+//     return (<div>
+//         <Box sx={{flexGrow: 1}}>
+//             <div className={classes1.comp_disp}>
+//                 <div className='part1'>
+//             <Typography variant="h3" component="div" gutterBottom sx={{ color: '#FFA900' }}>
+//                 <strong>DARE TO ENTER</strong>
+//             </Typography>
+//             <Button className={`'btn_prop' ${classes1.btn_prop}`}>
+//                 <Typography variant="h5" component="div">
+//                 JOIN OUR DISCORD
+//                 </Typography>
+//             </Button></div>
             
-            </div>
-        </Box>
-    </div>)
-}
-const Follow = () => {
-    const classes2 = useStyles();
-    return (<div>
-        <Box sx={{flexGrow: 1}}>
-            <Typography variant="h3" component="div" gutterBottom sx={{ color: '#FFA900' }}>
-                <strong>FOLLOW US</strong>
-            </Typography>
-            <div className={classes2.btn_disp}>
-            <Button>
-                <TwitterIcon fontSize='large' className={classes2.icon_prop}/>
-            </Button>
-            <Button>
-                <LinkedInIcon fontSize='large' className={classes2.icon_prop}/>
-            </Button>
-            <Button>
-                <InstagramIcon fontSize='large' className={classes2.icon_prop}/>
-            </Button></div>
-        </Box>
-    </div>)
-}
+//             </div>
+//         </Box>
+//     </div>)
+// }
+// const Follow = () => {
+//     const classes2 = useStyles();
+//     return (<div>
+//         <Box sx={{flexGrow: 1}}>
+//             <Typography variant="h3" component="div" gutterBottom sx={{ color: '#FFA900' }}>
+//                 <strong>FOLLOW US</strong>
+//             </Typography>
+//             <div className={classes2.btn_disp}>
+//             <Button>
+//                 <TwitterIcon fontSize='large' className={classes2.icon_prop}/>
+//             </Button>
+//             <Button>
+//                 <LinkedInIcon fontSize='large' className={classes2.icon_prop}/>
+//             </Button>
+//             <Button>
+//                 <InstagramIcon fontSize='large' className={classes2.icon_prop}/>
+//             </Button></div>
+//         </Box>
+//     </div>)
+// }
 const Footer = () => {
     
     return (<div>
